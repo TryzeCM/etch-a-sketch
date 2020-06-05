412d810b5822f1784a42ce785a8e8a2137ab7b40
Colin Merkert <c.merkert@outlook.de>
2020-06-05 15:52:32 +0200

    made grid resizable

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+/**********************TO DO ****************************/
+//make sure prompt closes
+//size must not be larger than 100
+//clear only button
+//random colors
+
+
 //Fill the Grid
 for (let i = 0; i <10000; i++) {
     let container = document.querySelector("#container");
@@ -33,12 +40,22 @@ resizeButton.addEventListener("click", () => {
             console.log(input);
         }
     }
-/*******************************TO DO*********************************
     resizeGrid(input);
+/*******************************TO DO*********************************
+    
  */
 });
 
 function getInput(text) {
     let input = prompt(text);
     return parseInt(input);
+}
+
+let test = document.querySelector("#change");
+test.setAttribute("style", "background-color: purple;");
+
+function resizeGrid(size) {
+    let container = document.querySelector("#container");
+    container.setAttribute("style", `grid-template-columns: repeat(${size}, 1fr);
+                                    grid-template-rows: repeat(${size}, 1fr)`);
 }
