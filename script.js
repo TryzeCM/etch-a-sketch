4128b3b1c3c09880cec91dec2a151e9b47796bce
Colin Merkert <c.merkert@outlook.de>
2020-06-05 15:30:02 +0200

    added a button event to clear the grid of the color and to ask for user input

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-for (let i = 0; i < 2500; i++) {
+//Fill the Grid
+for (let i = 0; i <10000; i++) {
     let container = document.querySelector("#container");
     let div = document.createElement("div");
     div.setAttribute("class", "interactable");
@@ -6,11 +7,38 @@ for (let i = 0; i < 2500; i++) {
 
 }
 
+//Draw on the Grid
 let interactables = document.querySelectorAll(".interactable");
-
 interactables.forEach((interactable) => {
     interactable.addEventListener("mouseenter", () => {
         interactable.setAttribute("class", "drawn")
     });
 });
 
+//Clears Grid
+let resizeButton = document.querySelector("#resize");
+resizeButton.addEventListener("click", () => {
+    interactables.forEach((interactable) => {
+        interactable.removeAttribute("class");
+    });
+
+    let input = getInput("Enter a size");
+    console.log(typeof(input) + ": " + input);
+    
+//Checks if input is valid
+    if (isNaN(input)) {
+
+        while(isNaN(input)) {
+            input = getInput("Please enter a Number.");
+            console.log(input);
+        }
+    }
+/*******************************TO DO*********************************
+    resizeGrid(input);
+ */
+});
+
+function getInput(text) {
+    let input = prompt(text);
+    return parseInt(input);
+}
